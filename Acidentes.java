1dd22752d6e66c5e38690f5bee5ce07d94ea6a08
Laysa Santiago <55203249+Laysanha@users.noreply.github.com>
2021-10-26 11:32:07 -0300

    Add files via upload

--- Acidentes.java
+++ Acidentes.java
@@ -1,5 +1,48 @@
 package laysasantiagoalfredo;
 
+import javax.swing.JOptionPane;
+
 public class Acidentes {
+	private Rodovia rodovia;
+	private Veiculo veiculo;
+	private int qtdVitimas;
 	
+	public Rodovia getRodovia() {
+		return rodovia;
+	}
+
+	public void setRodovia(Rodovia rodovia) {
+		this.rodovia = rodovia;
+	}
+
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
+
+	public void setVeiculo(Veiculo veiculo) {
+		this.veiculo = veiculo;
+	}
+
+	public int getQtdVitimas() {
+		return qtdVitimas;
+	}
+
+	public void setQtdVitimas(int qtdVitimas) {
+		this.qtdVitimas = qtdVitimas;
+	}
+	
+	public Acidentes() {
+//		this.rodovia = rodovia;
+//		this.veiculo = veiculo;
+		this.qtdVitimas = 0;
+	}
+	
+	public void registraAcidente(){
+		setQtdVitimas(Integer.parseInt(JOptionPane.showInputDialog("Nome do condutor:")));
+	}
+	
+	@Override
+	public String toString() {
+		return "Acidentes [rodovia=" + rodovia + ", veiculo=" + veiculo + ", qtdVitimas=" + qtdVitimas + "]";
+	}
 }
